0c96cec0d2f29d1c129a2f9bd1aef8ac09d71973
Joey Wiryawan <76517660+JoWi2404@users.noreply.github.com>
2024-12-27 13:53:21 +0700

    Added initial files

---
+++ UberSwiftProject/UberSwiftProject/App/UberSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftProjectApp.swift
+//  UberSwiftProject
+//
+//  Created by Joey Wiryawan on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ UberSwiftProject/UberSwiftProject/Core/View/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  UberSwiftProject
+//
+//  Created by Joey Wiryawan on 27/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
