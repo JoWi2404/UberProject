f3b73bfebf9bcfcad5540b6a72cd86beed3bd774
Joey Wiryawan <76517660+JoWi2404@users.noreply.github.com>
2024-12-27 13:48:01 +0700

    Upload Intial Files

---
+++ UberSwiftProject/UberSwiftProjectTests/UberSwiftProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftProjectTests.swift
+//  UberSwiftProjectTests
+//
+//  Created by Joey Wiryawan on 26/12/24.
+//
+
+import Testing
+@testable import UberSwiftProject
+
+struct UberSwiftProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
